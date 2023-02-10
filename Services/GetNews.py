5d203f208e2b54690c8e936b08418d27b7fd760e
blackflame576 <pws15548@gmail.com>
2023-02-10 22:09:18 +0300

    Updating #2

--- Services/GetNews.py
+++ Services/GetNews.py
@@ -6,8 +6,8 @@ import os
 
 class ParseNews:
     def __init__(self,ProjectDir) -> None:
-        self.NewsURL = "https://lenta.ru/"
-        self.itNewsURL = ""
+        self.NewsURL = "https://lenta.ru/parts/news"
+        self.itNewsURL = "https://3dnews.ru/news"
         self.NewsArray = []
         self.itNewsArray = []
         self.ProjectDir = ProjectDir
@@ -15,17 +15,26 @@ class ParseNews:
     def News(self):
         r = requests.get(self.NewsURL).text
         soup = BeautifulSoup(r, 'html.parser')
-        content = soup.find_all("span")
+        content = soup.find_all("h3",class_ = 'card-full-news__title')
         for span in content:
             text = span.text.replace('"','')
             # text = await self.FilteringTransforms(text,to_words=True)
             self.NewsArray.append(text)
-        file = open(self.ProjectDir + "/AssistantSettings/News.json","w")
+        file = open(os.path.join(self.ProjectDir,"AssistantSettings/News.json"),"w",encoding="utf-8")
         json.dump(self.NewsArray,file,indent=2,ensure_ascii=False,sort_keys=True)
         file.close()
 
     def it_news(self):
-        pass
+        r = requests.get(self.itNewsURL).text
+        soup = BeautifulSoup(r, 'html.parser')
+        content = soup.find_all("a",class_='entry-header')
+        for span in content:
+            text = span.text.replace('"','')
+            # text = await self.FilteringTransforms(text,to_words=True)
+            self.itNewsArray.append(text)
+        file = open(os.path.join(self.ProjectDir,"AssistantSettings/IT_News.json"),"w",encoding="utf-8")
+        json.dump(self.itNewsArray,file,ensure_ascii=False,sort_keys=True, indent=2)
+        file.close()
 
     def StartParse(self):
         self.News()
@@ -33,4 +42,5 @@ class ParseNews:
 
 if __name__ == "__main__":
     Parse = ParseNews(os.path.dirname(os.path.realpath(__file__)))
-    Parse.StartParse()
+    Parse.News()
+    # Parse.StartParse()
