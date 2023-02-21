030324ef1d1af2a970d2e123ee9b3b11086ec5c2
blackflame576 <pws15548@gmail.com>
2023-02-21 22:04:08 +0300

    Deploy

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from cx_Freeze import setup, Executable
+
+executables = [Executable('Artyom.py')]
+
+excludes = ['unicodedata', 'urllib','pytorch','silero''xml', 'bz2']
+
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'excludes': excludes,
+    }
+}
+
+setup(name='Artyom',
+      version='0.0.1',
+      description='Artyom - neural assistant',
+      executables=executables,
+      options=options)
